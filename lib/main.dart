96336ba32bddaa4f98d237aa8cca0ae38a1acf7a
Mohamed Lotfy <molotfy50@gmail.com>
2021-11-04 01:19:06 +0200

    initialize project

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:sign_language_interpreter/my_app.dart';
+
+void main() {
+  runApp(MyApp());
+}
---
+++ lib/my_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //! title: 'Flutter Demo', waiting then namong the app
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(),
+    );
+  }
+}
